f3d1ccc41c03e196a437e779b3406422ddff06fe
Sanjo <2730723@tcsgenailab.onmicrosoft.com>
2026-03-12 11:46:11 +0000

    v6

---
+++ s.sh
@@ -0,0 +1,5 @@
+Dependency & Build Management: Streamlined the build process by managing Maven (pom.xml) dependency lifecycles, ensuring version compatibility across microservices, and orchestrating the packaging and distribution of artifacts to JFrog Artifactory.
+
+Deployment Orchestration: Managed the end-to-end release cycle by triggering and monitoring automated jobs, ensuring successful deployment of containerized services to Kubernetes.
+
+Infrastructure Monitoring: Utilized K8s logs to conduct proactive compatibility audits, ensuring seamless integration between application code and the underlying cloud infrastructure.
